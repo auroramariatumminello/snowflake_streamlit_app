522ef25eb4427ac4d64704c725f0a80d9ba95830
TUMMINELLO <aurora.maria.tumminello.z@gmail.com>
2022-11-16 11:59:42 +0100

    Fruit dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,20 @@
+# LIBRARIES
 import streamlit
+import pandas
 
+# DATASETS
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list.set_index('Fruit', inplace=True)
+
+# STREAMLIT STRUCTURE
+# Section 1
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+# Section 2
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
