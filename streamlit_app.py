5c4d6c11de98fe0ce995810dd0f2f960e395c528
TUMMINELLO <aurora.maria.tumminello.z@gmail.com>
2022-11-17 15:01:25 +0100

    Pandas import

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 
 # DATASETS
 # List of fruits
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list.set_index('Fruit', inplace=True)
 
 # Information about the fruit
